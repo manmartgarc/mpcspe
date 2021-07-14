75d4c8556b5ff484d981d29e79038984cd531a40
Manuel Martinez <manmartgarc@gmail.com>
2021-07-14 12:56:22 -0400

    add code for movingday

---
+++ placement-exams/2018-19/movingday/movingday.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wednesday, 9th June 2021 11:29:44 pm
+===============================================================================
+@filename:  movingday.py
+@author:    Manuel Martinez (manmart@uchicago.edu)
+@project:   mpcspe | 2018-19
+@purpose:   movingday
+===============================================================================
+"""
