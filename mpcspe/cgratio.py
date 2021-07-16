8e1d53e138c92d2cffe4bf6673212daf770bfa62
Manuel Martinez <manmartgarc@gmail.com>
2021-07-16 19:27:58 -0400

    add work for cgratio

--- mpcspe/cgratio.py
+++ mpcspe/cgratio.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Friday, 16th July 2021 3:23:48 pm
+Created on Friday, 16th July 2021 7:13:19 pm
 ===============================================================================
-@filename:  batterup.py
+@filename:  cgratio.py
 @author:    Manuel Martinez (manmart@uchicago.edu)
 @project:   mpcspe | 2017-18
-@purpose:   batterup
+@purpose:   cgratio
 ===============================================================================
 """
 import sys
@@ -13,9 +13,16 @@ import sys
 
 def main(line: str) -> None:
     lines = line.splitlines()
-    inputs = list(map(int, lines[1].split()))
-    inputs = [i for i in inputs if i >= 0]
-    result = sum(inputs) / len(inputs)
+
+    total: int = 0
+    cgs: int = 0
+    for line in lines[1:]:
+        total += len(line)
+        cgs += line.count('C')
+        cgs += line.count('G')
+
+    result = (cgs / total) * 100
+
     sys.stdout.write(str(result))
 
 
--- tests/test_cgratio.py
+++ tests/test_cgratio.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Friday, 16th July 2021 3:32:24 pm
+Created on Friday, 16th July 2021 7:25:53 pm
 ===============================================================================
-@filename:  test_batterup.py
+@filename:  test_cgratio.py
 @author:    Manuel Martinez (manmart@uchicago.edu)
 @project:   mpcspe | 2017-18
-@purpose:   unit tests for batterup
+@purpose:   unit tests for cgratio
 ===============================================================================
 """
 import io
+import math
 from contextlib import redirect_stdout
 
 import pytest
+from mpcspe.cgratio import main
 from mpcspe.loaders import load_test_data
-from mpcspe.batterup import main
 
-TEST_DATA = load_test_data('batterup')
+TEST_DATA = load_test_data('cgratio')
 
 
 @pytest.mark.parametrize(('input', 'output'), TEST_DATA.items())
-def test_batterup(input, output):
+def test_cgratio(input, output):
     f = io.StringIO()
     output = output.strip()
     with redirect_stdout(f):
         main(input)
         s = f.getvalue()
-        assert output == s
+        assert math.isclose(float(output), float(s), rel_tol=10e-3)
