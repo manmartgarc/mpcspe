bb75fa80ec015d16d37d99849dc3c3dd4359b10b
Manuel Martinez <manmartgarc@gmail.com>
2021-07-16 00:20:28 -0400

    add movingday

--- mpcspe/movingday.py
+++ mpcspe/movingday.py
@@ -8,3 +8,31 @@ Created on Wednesday, 9th June 2021 11:29:44 pm
 @purpose:   movingday
 ===============================================================================
 """
+import sys
+from dataclasses import dataclass
+
+
+@dataclass
+class Box:
+    length: int
+    width: int
+    height: int
+
+    def get_volume(self) -> int:
+        return self.length * self.width * self.height
+
+
+def main(line: str):
+    lines = line.splitlines()
+    diffs = []
+    n, V = map(int, lines[0].split())
+    for line in lines[1:]:
+        l, w, h = map(int, line.split())
+        box = Box(length=l, width=w, height=h)
+        diffs.append(box.get_volume() - V)
+    assert n == len(diffs)
+    sys.stdout.write(str(max(diffs)))
+
+
+if __name__ == "__main__":
+    main(sys.stdin.read())
---
+++ tests/test_movingday.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thursday, 15th July 2021 11:36:13 pm
+===============================================================================
+@filename:  test_movingday.py
+@author:    Manuel Martinez (manmart@uchicago.edu)
+@project:   enter project name
+@purpose:   enter purpose
+===============================================================================
+"""
+import io
+from contextlib import redirect_stdout
+
+import pytest
+from mpcspe.loaders import load_test_data
+from mpcspe.movingday import main
+
+TEST_DATA = load_test_data('movingday')
+
+
+@pytest.mark.parametrize(('input', 'output'), TEST_DATA.items())
+def test_movingday(input, output):
+    f = io.StringIO()
+    output = int(output.strip())
+    with redirect_stdout(f):
+        main(input)
+        s = int(f.getvalue())
+        assert output == s
